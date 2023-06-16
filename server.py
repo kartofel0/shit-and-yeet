1e7edfb3e9b429bf69525fdcbb52ebd0153caf6d
kartofel0 <128752278+kartofel0@users.noreply.github.com>
2023-06-17 02:06:47 +0300

    server.py

--- server.py
+++ server.py
@@ -14,8 +14,23 @@ WINDOW_HEIGHT = 900
 
 playerNum = 0
 
-pijen = Pijen(WINDOW_WIDTH/2, WINDOW_HEIGHT/5, 3, 5)
-person = Player(WINDOW_WIDTH/2, WINDOW_HEIGHT-100, 3.3)
+pjn_vel = 3
+hmn_vel = 3.3
+
+pijen = Pijen(WINDOW_WIDTH/2, WINDOW_HEIGHT/5, pjn_vel, 5)
+human = Player(WINDOW_WIDTH/2, WINDOW_HEIGHT-100, hmn_vel)
+
+shitList = []
+
+
+#def endGame()
+
+
+
+
+
+
+
 
 server_soc = socket.socket()
 server_soc.bind((IP_ADDR, PORT))   #whoever tries to connect to that ip connects to that port
@@ -33,16 +48,66 @@ while playerNum < 2:
 
     # send window dimension, who are you
     if playerNum == 0:
-        client_soc.send('{WINDOW_WIDTH},{WINDOW_HEIGHT},pjn'.encode())
+        client_soc.send('pjn,{pjn_vel}'.encode())
         playerNum += 1
     elif playerNum == 1:
-        client_soc.send('{WINDOW_WIDTH},{WINDOW_HEIGHT},hmn'.encode())
+        client_soc.send('hmn,{hmn_vel}'.encode())
         playerNum += 1
 
 
-# sign to begin game
-client_soc.send('ok'.encode())
+# now begin game
+client_soc.send('start'.encode())
 
+
+# main game loop
+winner = 'tie'    # when disconnected ig
+run = True
+while run:
+    # data: ['???,mov,addX,pWidth']
+    # data: ['pjn,sht']
+    # data: ['hmn,hit']
+    # data: ['???,end']
+    data = client_soc.recv(1024).decode()
+    data = data.split(',')
+    #dataLen = len(data)
+    type = data[0]
+    action = data[1]
+    try:
+        addX = data[2]
+        pWidth = data[3]
+    except:
+        pass
+    
+
+
+    match action:
+
+        case 'mov':
+            if type == 'pjn':
+                pijen.updatePos(addX, 0, pWidth, WINDOW_WIDTH)
+                reply = str(pijen.getPosX())
+            elif type == 'hmn':
+                human.updatePos(addX, 0, pWidth, WINDOW_WIDTH)
+                reply = str(human.getPosX())
+            # return updated pos
+            client_soc.send(reply.encode())
+        
+        case 'sht':
+            pass
+            # create shit object, add to shitList as first
+            # send to client position
+
+        case 'rmv':    # shit reached floor, eliminate shit
+            pass
+            # remove first shit from list
+
+        case 'hit':
+            pass    # run false, winner = pjn wins
+
+        case 'end':    # timer ended /  no more poops from pijen
+            pass    # run false, winner = hmn wins
+
+# endGame(winner)
 
 
 
