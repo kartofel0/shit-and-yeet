f31f0a3a9e410b40909b5cd6cf50f8cfa859c4f3
kartofel0 <128752278+kartofel0@users.noreply.github.com>
2023-06-17 02:23:56 +0300

    Update client.py

--- client.py
+++ client.py
@@ -9,25 +9,45 @@ data = my_socket.recv(1024).decode()
 player, vel = data.split(',')
 #print('The server sent: ' + data)
 
-def move(self, width):
+def Move():
     moveX = 0
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        moveX -= vel
-        
+        moveX -= vel 
     if keys[pygame.K_RIGHT]:
         moveX += vel
+    
     my_socket.send('{player},{moveX}'.encode())
+    # recieve updated pos, draw
+
+def Drop():    # if pijen
+    # if pressed shift send 'sht' etc
+    pass
+
+def EliminateShit():
+    # if floor collides with st \/ send 'rmv'
+    pass
+
+def ShatOn():    # if human
+    # if collided with st (/more than on object (floor + shit)) send 'hit' etc
+    pass
+
+
+# if sendning 'hit' or 'end'
 
 def main( ):
     run = True
     while run:
-        newX = my_socket.recv(1024).decode()
-    if player == 'hmn':
-        my_socket.send('hit'.encode())
-    if player == 'pjn':
-        my_socket.send('sht'.encode())
-        shtLoc = my_socket.recv(1024).decode()
+        pass
 
-my_socket.send('end'.encode())
-my_socket.close()
+
+#        newX = my_socket.recv(1024).decode()
+#    if player == 'hmn':
+#        my_socket.send('hit'.encode())
+#    if player == 'pjn':
+#        my_socket.send('sht'.encode())
+#        shtLoc = my_socket.recv(1024).decode()
+
+#my_socket.send('end'.encode())
+#my_socket.close()
