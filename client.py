204272139cab4a95b61096087f12edd88f4a917a
Milyaga <135754675+Milyaga@users.noreply.github.com>
2023-06-17 01:47:58 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,33 @@
 import socket
+import pygame
 
 my_socket = socket.socket()
 my_socket.connect(('192.168.0.181', 5555))  #connects to a socket in that location
 
-my_socket.send('Omer'.encode())
+#my_socket.send('Omer'.encode())
 data = my_socket.recv(1024).decode()
-print('The server sent: ' + data)
+player, vel = data.split(',')
+#print('The server sent: ' + data)
 
+def move(self, width):
+    moveX = 0
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        moveX -= vel
+        
+    if keys[pygame.K_RIGHT]:
+        moveX += vel
+    my_socket.send('{player},{moveX}'.encode())
+
+def main( ):
+    run = True
+    while run:
+        newX = my_socket.recv(1024).decode()
+    if player == 'hmn':
+        my_socket.send('hit'.encode())
+    if player == 'pjn':
+        my_socket.send('sht'.encode())
+        shtLoc = my_socket.recv(1024).decode()
+
+my_socket.send('end'.encode())
 my_socket.close()
