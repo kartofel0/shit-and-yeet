826e264c6eac4793cba34e4dfe7f329c8a53cc98
Milyaga <135754675+Milyaga@users.noreply.github.com>
2023-06-17 00:52:11 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 my_socket = socket.socket()
-my_socket.connect(('192.168.0.181', 8820))  #connects to a socket in that location
+my_socket.connect(('192.168.0.181', 5555))  #connects to a socket in that location
 
 my_socket.send('Omer'.encode())
 data = my_socket.recv(1024).decode()
