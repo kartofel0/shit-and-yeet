921390fe195437bd187c3e59e2fd701c243c8706
kartofel0 <128752278+kartofel0@users.noreply.github.com>
2023-06-17 12:35:36 +0300

    Add files via upload

---
+++ movingSprites.py
@@ -0,0 +1,40 @@
+import pygame
+
+class spritesheet(object):
+    def __init__(self, filename):
+        try:
+            #self.sheet = pygame.image.load(filename).convert()
+            self.sheet = pygame.image.load(filename)
+        except pygame.error:
+            print('Unable to load spritesheet image:', filename)
+
+    
+    def image_at(self, rectangle, colorkey = None):
+        "Loads image from x,y,x+offset,y+offset"
+        rect = pygame.Rect(rectangle)
+        #image = pygame.Surface(rect.size).convert()
+        image = pygame.Surface(rect.size)
+        image.blit(self.sheet, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey == -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image
+    
+    def images_at(self, rects, colorkey = None):
+        "Loads multiple images, supply a list of coordinates" 
+        return [self.image_at(rect, colorkey) for rect in rects]
+
+    def load_strip(self, rect, image_count, colorkey = None):
+        "Loads a strip of images and returns them as a list"
+        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+                for x in range(image_count)]
+        return self.images_at(tups, colorkey)
+
+
+class MovingSprites():
+    def __init__(self, spritesheet_name, width, height, amount):
+        self.spritesheet = spritesheet(spritesheet_name)
+        self.images = []
+        #
+        # spritesheet, width, height, amount
