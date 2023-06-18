043f4651e3faf313c08dc5a7b041f1c8c723c4fe
kartofel0 <128752278+kartofel0@users.noreply.github.com>
2023-06-18 19:41:35 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,14 +1,28 @@
 class Player():
-    def __init__(self, x, y, vel=0):
+    def __init__(self, x, y, vel, anim, direction):
         self.x = x
         self.y = y
         self.vel = vel
+        self.anim = anim
+        self.direction = direction
 
     def getPosX(self):
         return self.x
     
     def getPosY(self):
         return self.y
+    
+    def getAnim(self):
+        return self.anim
+    
+    def getDir(self):
+        return self.direction
+    
+    def setAnim(self, a):
+        self.anim = a
+    
+    def setDir(self, d):
+        self.direction = d
     
     def updatePos(self, addX, addY, pWidth=0, winWidth=0):
         self.x += float(addX)
