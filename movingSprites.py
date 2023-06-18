2b364a7400f25e3a259f4e5b2b5a881f609dca6d
kartofel0 <128752278+kartofel0@users.noreply.github.com>
2023-06-18 08:47:32 +0300

    Update movingSprites.py

--- movingSprites.py
+++ movingSprites.py
@@ -1,40 +1,85 @@
 import pygame
 
-class spritesheet(object):
-    def __init__(self, filename):
-        try:
-            #self.sheet = pygame.image.load(filename).convert()
-            self.sheet = pygame.image.load(filename)
-        except pygame.error:
-            print('Unable to load spritesheet image:', filename)
-
-    
-    def image_at(self, rectangle, colorkey = None):
-        "Loads image from x,y,x+offset,y+offset"
-        rect = pygame.Rect(rectangle)
-        #image = pygame.Surface(rect.size).convert()
-        image = pygame.Surface(rect.size)
-        image.blit(self.sheet, (0, 0), rect)
-        if colorkey is not None:
-            if colorkey == -1:
-                colorkey = image.get_at((0,0))
-            image.set_colorkey(colorkey, pygame.RLEACCEL)
-        return image
-    
-    def images_at(self, rects, colorkey = None):
-        "Loads multiple images, supply a list of coordinates" 
-        return [self.image_at(rect, colorkey) for rect in rects]
-
-    def load_strip(self, rect, image_count, colorkey = None):
-        "Loads a strip of images and returns them as a list"
-        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
-                for x in range(image_count)]
-        return self.images_at(tups, colorkey)
-
-
 class MovingSprites():
     def __init__(self, spritesheet_name, width, height, amount):
-        self.spritesheet = spritesheet(spritesheet_name)
+        #self.spritesheet = pygame.image.load(spritesheet_name).convert()
+        self.spritesheet = pygame.image.load(spritesheet_name).convert_alpha()
+        self.spritesheet.set_colorkey((0,0,0))
+        self.ssw = width
+        self.ssh = height
+        self.amount = amount
+        '''
         self.images = []
-        #
-        # spritesheet, width, height, amount
+        for i in range(0, self.amount):
+            img = self.getImage(i*(self.ssw/self.amount), 0, self.ssw/self.amount, self.ssh)
+            self.images.append(img)
+        print('list len of sprites: ' + str(len(self.images)))
+        '''
+        self.images = self.divideSpritesheet(0, 0, self.ssw/amount, self.ssh, amount)
+        #print(len(self.images))
+        self.cur_sprite = 0
+        #self.image = self.images[self.cur_sprite]
+        self.image = self.images[2] ########### change to 3 already
+        self.image = pygame.transform.scale_by(self.image, 0.15)
+        self.rect = self.image.get_rect()
+
+
+    def divideSpritesheet(self, sprite_start_x,sprite_start_y,sprite_size_x,sprite_size_y,sheet_frames):
+    #def slice_sheet(self,sprite_sheet,sprite_start_x,sprite_start_y,sprite_size_x,sprite_size_y,sheet_frames):
+        frames = []
+        sheet_start = 1 
+        #frame_pos = 0
+        sprite_start_x_base = sprite_size_x
+        while sheet_start <= sheet_frames:
+            sheet = self.spritesheet
+            sheet.set_clip(pygame.Rect(sprite_start_x,sprite_start_y,sprite_size_x,sprite_size_y))
+            sprite_clip = sheet.subsurface(sheet.get_clip())
+            frames.append(sprite_clip)
+            sprite_start_x += sprite_start_x_base
+            sheet_start += 1
+        return frames
+
+    def getRect(self):
+        return self.rect
+    
+    def getPosY(self):
+        return self.y
+
+
+    def getImage(self, posX, posY, width, height):
+        image = pygame.Surface((width, height))
+        image.blit(self.spritesheet, (0, 0), (posX, posY, width, height))
+        image.set_colorkey((0,0,0))
+        return image
+    
+
+    def update(self, anim, scale, dir):
+        if anim:
+            self.cur_sprite += 0.05
+            if int(self.cur_sprite) == self.amount-1:
+                self.cur_sprite = 0
+        else:
+            self.cur_sprite = self.amount-1
+
+        print('current sprite num: ' + str(int(self.cur_sprite+1)) + ' out of ' + str(len(self.images)))
+
+        self.image = self.images[int(self.cur_sprite)]
+        self.image = pygame.transform.scale_by(self.image, scale)
+        if dir == 'r':
+            self.image = pygame.transform.flip(self.image, True, False)
+        self.rect = self.image.get_rect()
+
+
+    def draw(self, win, x, y, anim, scale, dir=None):
+        #self.x = float(x)
+        self.y = float(y)
+        x = float(x)
+        y = float(y)
+        #self.update(anim, scale, dir)  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        win.blit(self.image, (x,y))  # problem with x
+
+
+# in draw() uncomment update() line 77, delete line 23
+# uncomment line 21, delete line 22
+
+# line 22 !!!
