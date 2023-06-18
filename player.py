62a03397e36d61cf244e800d22761cc600ae90e6
kartofel0 <128752278+kartofel0@users.noreply.github.com>
2023-06-18 08:47:54 +0300

    Update player.py

--- player.py
+++ player.py
@@ -7,10 +7,13 @@ class Player():
     def getPosX(self):
         return self.x
     
+    def getPosY(self):
+        return self.y
+    
     def updatePos(self, addX, addY, pWidth=0, winWidth=0):
-        self.x += addX
-        self.y += addY
-        if self.x < pWidth/2:
-            self.x = pWidth/2
-        elif self.x > winWidth - pWidth/2:
-            self.x = winWidth - pWidth/2
+        self.x += float(addX)
+        self.y += float(addY)
+        if self.x <= 0:
+            self.x = 0
+        elif self.x > winWidth - pWidth:
+            self.x = winWidth - pWidth
